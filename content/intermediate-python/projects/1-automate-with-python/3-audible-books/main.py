0fce7b898d101705e04f9a89d43e0bc009a4e6f9
Vedat Kocoglu <vedattkocogluu@gmail.com>
2023-10-19 22:56:51 +0300

    Add new project to intermediate-python

--- content/intermediate-python/projects/1-automate-with-python/3-audible-books/main.py
+++ content/intermediate-python/projects/1-automate-with-python/3-audible-books/main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
 import logging
+import json
 
 SCROLL_PAUSE_TIME = 1
 
@@ -33,15 +34,33 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(url=url)
 
-scrape_page = True
+book_list = []
+scrape_open = True
+index = 1
+while scrape_open:
+    containers = driver.find_elements(by='xpath', value='//*[@id="center-3"]/div/div/div/span[2]/ul')
+    for container in containers:
+        books = container.find_elements(by='xpath', value=f'./li[{index}]/div/div[1]/div/div[2]/div/div/span/ul')
+        for book in books:
+            name = book.find_element(by='xpath', value='./li[1]/h3/a').text
+            link = book.find_element(by='xpath', value='./li[1]/h3/a').get_attribute("href")
+            sub_title = book.find_element(by='xpath', value='./li[2]/span').text
+            author = book.find_element(by='xpath', value='./li[3]/span/a').text
+            author_link = book.find_element(by='xpath', value='./li[3]/span/a').get_attribute("href")
+            book_list.append({
+                "name": name,
+                "link": link,
+                "sub_title": sub_title,
+                "author": author,
+                "author_links": author_link,
+            })
+        index += 1    
+        if index > 20:
+            click_next_page()
+            index = 1 
 
-while scrape_page:
-    scroll_down(4)
-    click_next_page()
-
-# containers = driver.find_element()
-
-
+        with open("book_list.json", "w") as file:
+            json.dump(book_list, file, indent=4)
 
 driver.quit()
 
